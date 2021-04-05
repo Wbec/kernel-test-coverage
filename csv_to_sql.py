76da8584b20b7526104fb42342963fd4965fdda9
Will Beckett <wbeckett@uvic.ca>
2021-04-05 00:51:27 -0700

    make csv_to_sql accept variable source directories

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -6,10 +6,6 @@ import sqlite3
 from locations import blame_parsed, OUTDIR
 
 
-connection = sqlite3.connect(OUTDIR / "function_survey.db")
-cursor = connection.cursor()
-
-
 def read_csv(filename):
     with open(filename) as f:
         reader = csv.reader(f)
@@ -17,40 +13,48 @@ def read_csv(filename):
             yield row
 
 
-def corresponding_kernel_file(filename):
+def corresponding_kernel_file(filename, source_dir):
     """Get the path to the kernel file corresponding to parsed intermediary output file."""
-    return str(filename.relative_to(blame_parsed).with_suffix(""))
+    return str(filename.relative_to(source_dir).with_suffix(""))
 
 
-def reset_table(table_name, suffix, column_names):
+def reset_table(table_name, suffix, column_names, connection, source_dir):
+    cursor = connection.cursor()
     cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
     cursor.execute(f"CREATE TABLE {table_name} ({','.join(column_names)})")
     placeholder = ", ".join(["?"] * len(column_names))
     cursor.executemany(
         f"INSERT INTO {table_name} VALUES ({placeholder})",
         (
-            (corresponding_kernel_file(filename), *row)
+            (corresponding_kernel_file(filename, source_dir), *row)
             # assumes no extra commas in file contents. Will error if this is not true.
-            for filename in blame_parsed.rglob(f"*.c.{suffix}")
+            for filename in source_dir.rglob(f"*.c.{suffix}")
             for row in read_csv(filename)
         ),
     )
     connection.commit()
 
 
-def reset_all():
-    reset_table("cregit_functions", "functions", ("file", "name"))
+def reset_all(source_dir, outdir):
+    connection = sqlite3.connect(outdir / "function_survey.db")
+
+    for table_name, suffix, column_names in (
+        ("cregit_functions", "functions", ("file", "name")),
+        ("cregit_calls", "calls", ("file", "caller", "callee")),
+        ("cregit_includes", "includes", ("file", "include")),
+        ("cregit_specifiers", "specifiers", ("file", "name", "specifier")),
+        ("cregit_identifiers", "names", ("file", "function", "identifier")),
+        ("cregit_macros", "macros", ("file", "name")),
+    ):
+        reset_table(table_name, suffix, column_names, connection, source_dir)
+    cursor = connection.cursor()
     cursor.execute("CREATE INDEX idx_functions ON cregit_functions (file, name);")
-    reset_table("cregit_calls", "calls", ("file", "caller", "callee"))
     cursor.execute("CREATE INDEX idx_calls ON cregit_calls (file,caller,callee);")
-    reset_table("cregit_includes", "includes", ("file", "include"))
-    reset_table("cregit_specifiers", "specifiers", ("file", "name", "specifier"))
-    reset_table("cregit_identifiers", "names", ("file", "function", "identifier"))
     cursor.execute(
         "CREATE INDEX idx_identifiers ON cregit_identifiers (file,function,identifier);"
     )
-    reset_table("cregit_macros", "macros", ("file", "name"))
+    connection.commit()
 
 
 if __name__ == "__main__":
-    reset_all()
+    reset_all(blame_parsed, OUTDIR)
